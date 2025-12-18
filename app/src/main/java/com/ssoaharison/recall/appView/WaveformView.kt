603b287d87ef829c69522e324e7c9c1618a9b620
StebaneJhon <111849849+StebaneJhon@users.noreply.github.com>
2025-12-19 00:28:56 +0100

    Audio recorder dialog

---
+++ app/src/main/java/com/ssoaharison/recall/appView/WaveformView.kt
@@ -0,0 +1,69 @@
+package com.ssoaharison.recall.appView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class WaveformView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+    private var paint = Paint()
+    private var amplitudes = ArrayList<Float>()
+    private var spikes = ArrayList<RectF>()
+
+    private var radius = 6f
+    private var w = 9f
+    private var d = 6f
+
+    private var screenWidth = 0f
+    private var screenHeight = 400f
+
+    private var maxSpikes = 0
+
+    init {
+        paint.color = Color.rgb(12, 10, 9)
+
+        screenWidth = resources.displayMetrics.widthPixels.toFloat()
+
+        maxSpikes = (screenWidth / (w+d)).toInt()
+    }
+
+    fun addAmplitude(amp: Float) {
+        var norm = Math.min(amp.toInt()/7, 400).toFloat()
+        amplitudes.add(norm)
+
+        spikes.clear()
+        var amps = amplitudes.takeLast(maxSpikes)
+        for (i in amps.indices) {
+            var left = screenWidth - i*(w+d)
+            var top = screenHeight/2 - amps[i]/2
+            var right = left + w
+            var bottom = top + amps[i]
+
+            spikes.add(RectF(left, top, right, bottom))
+        }
+
+
+        invalidate() // To trigger the draw method
+    }
+
+    fun clear(): ArrayList<Float> {
+        val amps = amplitudes.clone() as ArrayList<Float>
+        amplitudes.clear()
+        spikes.clear()
+        invalidate()
+
+        return amps
+    }
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        spikes.forEach {
+            canvas.drawRoundRect(it, radius, radius, paint)
+        }
+    }
+
+}
